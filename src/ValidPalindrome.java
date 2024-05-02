274e58bdec5ed84ab19dee56366d4bd48fd954a2
Omar Shalaby <47595444+OmarShalaby24@users.noreply.github.com>
2024-05-02 18:52:36 +0300

    Valid Palindrome (Easy)

--- src/ValidPalindrome.java
+++ src/ValidPalindrome.java
@@ -1,8 +1,8 @@
 public class ValidPalindrome {
     public static void main(String[] args) {
-//        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
-//        System.out.println(isPalindrome("race a car"));
-//        System.out.println(isPalindrome(" "));
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(isPalindrome("race a car"));
+        System.out.println(isPalindrome(" "));
         System.out.println(isPalindrome("0P"));
     }
 
