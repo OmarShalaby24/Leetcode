8308262ce83ae48bb68d82f322a8f60eeb011eab
Omar Shalaby <47595444+OmarShalaby24@users.noreply.github.com>
2024-05-12 16:44:36 +0300

    Largest Local Values in a Matrix (Easy)

---
+++ src/LargestLocalValuesInMatrix.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class LargestLocalValuesInMatrix {
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(largestLocal(new int[][]{{9, 9, 8, 1}, {5, 6, 2, 6}, {8,2,6,4},{6,2,2,2}})));
+    }
+
+    public static int[][] largestLocal(int[][] grid){
+        int n = grid.length;
+        int[][] result = new int[n-2][n-2];
+
+        for(int i=1; i<n-1; i++){
+            for(int j=1; j<n-1; j++){
+                int temp = 0;
+                for(int k=i-1; k<=i+1; k++){
+                    for(int l=j-1; l<=j+1; l++){
+                        temp = Math.max(temp, grid[k][l]);
+                    }
+                }
+                result[i-1][j-1] = temp;
+            }
+        }
+
+        return result;
+    }
+}
