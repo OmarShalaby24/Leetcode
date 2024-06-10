947d6a5ce86f9a61ba48bc2127fcdb07789d4ace
OmarShalaby24 <ramo_24@outlook.com>
2024-06-11 00:07:59 +0300

    Height Checker (Easy)

--- src/HeightChecker.java
+++ src/HeightChecker.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+
 public class HeightChecker {
     public static void main(String[] args) {
+        System.out.println(heightChecker(new int[]{1,1,4,2,1,3}));
+    }
 
+    public static int heightChecker(int []heights){
+        int[] clone = Arrays.copyOf(heights, heights.length);
+        Arrays.sort(heights);
+        int count = 0;
+        for(int i=0; i<heights.length; i++){
+            if(heights[i] != clone[i]) count++;
+        }
+        return count;
     }
 }
