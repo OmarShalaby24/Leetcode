8ac1ef67b5da26b9e180a9ce145c3c5b5c80800e
OmarShalaby24 <ramo_24@outlook.com>
2024-06-19 23:50:47 +0300

    Minimum Number of Days to Make m Bouquets (Medium)

---
+++ src/MinimumNumberofDaystoMakemBouquets.java
@@ -0,0 +1,10 @@
+public class MinimumNumberofDaystoMakemBouquets {
+    public static void main(String[] args) {
+        System.out.println(minDays(new int[]{1,10,3,10,2}, 3, 2));
+    }
+
+    public static int minDays(int[] bloomDay, int m, int k) {
+
+        return 0;
+    }
+}
