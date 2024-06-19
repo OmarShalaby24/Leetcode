d3381c2621b3ddb8ce7ec1e5a9fa370bd406c7ab
OmarShalaby24 <ramo_24@outlook.com>
2024-06-19 23:58:23 +0300

    Minimum Number of Days to Make m Bouquets (Medium)

--- src/MinimumNumberofDaystoMakemBouquets.java
+++ src/MinimumNumberofDaystoMakemBouquets.java
@@ -4,7 +4,29 @@ public class MinimumNumberofDaystoMakemBouquets {
     }
 
     public static int minDays(int[] bloomDay, int m, int k) {
-
-        return 0;
+        int l = 1, r = 1000000000;
+        int ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int consecutiveLength = 0, bouquets = 0;
+            for (int j : bloomDay) {
+                if (j <= mid) {
+                    consecutiveLength++;
+                    if (consecutiveLength >= k) {
+                        consecutiveLength = 0;
+                        bouquets++;
+                    }
+                } else {
+                    consecutiveLength = 0;
+                }
+            }
+            if (bouquets >= m) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return ans;
     }
 }
